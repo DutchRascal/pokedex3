d342ac14d9336b1f533c2ff80a311314f0ee792d
DutchRascal <Andre@imac.fritz.box>
2017-02-04 19:36:13 +0100

    06 - 106

---
+++ pokedex3/PokeCell.swift
@@ -0,0 +1,34 @@
+//
+//  PokeCell.swift
+//  pokedex3
+//
+//  Created by Andre Boevink on 04/02/2017.
+//  Copyright © 2017 Andre Boevink. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+        
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+    
+}
